fc4ad0773a8937c5f6ec1cf362659506ed757fce
Bui Khac Nam <buikhacnam11@gmail.com>
2020-08-20 17:18:26 +0700

    ok

--- workout.js
+++ workout.js
@@ -1,12 +1,8 @@
-/*
-<body>
-	<h3>Second left: <span id='time-left'>5</span></h3>
-	<h3>Reps: <span id='rep'>0</span></h3>
-	<button id='start-button'>Start</button>
-	<button id='restart-button'>Restart</button>
-	<script type="text/javascript" src="workout.js"></script>
-</body>
-*/
+
+
+
+
+
  
 let quotes = ['"The last three or four reps is what makes the muscle grow."- Arnold Schwarzenegger', '"All progress takes place outside the comfort zone."-Michael John Bobak', '"Success usually comes to those who are too busy to be looking for it."-Henry David Thoreau', '"If you think lifting is dangerous, try being weak. Being weak is dangerous."-Bret Contreras', '"The only place where success comes before work is in the dictionary."-Vidal Sassoon' ]
 
@@ -17,36 +13,78 @@ let quotes = ['"The last three or four reps is what makes the muscle grow."- Arn
   const restartBtn = document.querySelector('#restart-button');
   const reps = document.querySelector('#rep');
   const totalTime = document.querySelector('#total-time');
-  let timeLeft = 90;
+  const changed = document.querySelector('#changed');
+  let timeLeft;
   let repCount = 0;
   let total = 0;
+  
+  
 
-  /*
+
   function countDown() {
-  
-  	quoteGenerator();
-
-  	repCount = repCount + 1;
+    this.disabled = true;
+    
+    let initalTimeLeft = timeLeft;
+    
+    quoteGenerator();
+    if (timeLeft > 0) {
+    repCount = repCount + 1;
     reps.innerHTML = repCount;
+  } 
 
-  	let inteval = setInterval(() => {
-  		timeLeftDisplay.innerHTML = timeLeft;
-  		timeLeft -= 1;
-  	}, 1000)
-  	setTimeout(() => { 
-  		clearInterval(inteval); 
-  		timeLeft = 5; 
-  	}, 6000);
-  	
-
-  
+    let inteval = setInterval(() => {
+      initalTimeLeft -= 1;
+      timeLeftDisplay.innerHTML = initalTimeLeft;
+      
+      
+    }, 1000)
+    setTimeout(() => { 
+      clearInterval(inteval); 
+       //timeLeft = initalTimeLeft;
+      this.disabled = false;
+    }, (initalTimeLeft * 1000));
   }
 
   startBtn.addEventListener('click', countDown);
 
-  */
 
-   startBtn.addEventListener('click', function countDown() {
+
+function handleFireButton() {
+  var guessInput = document.getElementById("rest-time");
+  var guess = guessInput.value;
+  timeLeft = guess;
+  if (guess !== "") {
+  changed.innerHTML = guess;
+}
+}
+
+
+function init() {
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+}
+
+function quoteGenerator() {
+  let newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return quoteGen.innerHTML = newQuote;
+ }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    quoteGenerator();
+  });
+
+window.onload = init;
+
+
+///////
+
+//<input id="btnSubmit" type="submit" onclick="this.disabled = true">
+//event.preventDefault(); startBtn.syle.pointer-events = 'none'; startBtn.syle.pointer-events = 'auto';
+
+/*
+  startBtn.onclick = function() {
     this.disabled = true;
     quoteGenerator();
 
@@ -55,36 +93,19 @@ let quotes = ['"The last three or four reps is what makes the muscle grow."- Arn
 
     let inteval = setInterval(() => {
       timeLeft -= 1;
-      timeLeftDisplay.innerHTML = timeLeft;   
+      timeLeftDisplay.innerHTML = timeLeft;
+      
       
     }, 1000)
     setTimeout(() => { 
       clearInterval(inteval); 
-      timeLeft = 90; 
+      timeLeft = 5; 
       this.disabled = false;
-    }, 90000);
+    }, 5000);
     
-
-  
-  });
+  }
  
- 
- 
- function quoteGenerator() {
- 	let newQuote = quotes[Math.floor(Math.random() * quotes.length)];
- 	return quoteGen.innerHTML = newQuote;
- }
-
-function initialQuote() {
-	let newQuote = quotes[Math.floor(Math.random() * quotes.length)];
- 	return quoteGen.innerHTML = newQuote;
-};
-
-
-
-///////
-
-
+*/
 
 
 
